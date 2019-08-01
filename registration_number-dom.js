d9fc53ec1787162be8a6d8f245c9f9f53e6c756d
FidelDhlamini <goodkidfidel@gmail.com>
2019-08-01 16:46:04 +0200

    added DOM code

--- registration_number-dom.js
+++ registration_number-dom.js
@@ -1,16 +1,20 @@
-var userInput = document.querySelector(".regInput");
-var addButton = document.querySelector(".addButton");
-var radioButtons = document.querySelector(".townName");
-var radio = document.querySelector(".whichTown");
-var display = document.querySelector(".displayPlate")
-var instance = RegistrationCheck();
+const userInput = document.querySelector(".regInput");
+const townSelector = document.querySelector(".selectTown");
+const addButton = document.querySelector(".addButton");
+const display = document.querySelector(".plates");
+displayError = document.querySelector(".error");
+const instance = RegistrationCheck();
 function sortRegistrations(){
+    const selectedTown = townSelector.value
+    const licenceTyped = userInput.value
+    if (selectedTown === ""){
+       display.innerHTML = "Select a town from the list" 
+    }
+    if (licenceTyped ===""){
+        displayError.innerHTML = "Type in a registration number"
+    }
 
-if (radio){
- var city = document.querySelector("input[name='townName']:checked");
- var licenceDisplay = instance.town(userInput.value)
- display.innerHTML = licenceDisplay;
-}
+
 
 
 
--- registration_number.js
+++ registration_number.js
@@ -1,43 +1,5 @@
 function RegistrationCheck() {
 
-    function whichTown(reg) {
 
-
-        var platesForAllTowns = [reg];
-        var paarlPlates = reg.filter(word => word.startsWith("CJ"))
-        var capeTownPlates = reg.filter(word => word.startsWith("CA"));
-        var bellvillePlates = reg.filter(word => word.startsWith("CJ"));
-        /*  for (var i = 0; i < platesForAllTowns.length; i++) {
-              if (platesForAllTowns[i].startsWith('CL')) {
-                  stellenbosch.push(plates[i]);
-
-              }
-              if (platesForAllTowns[i].startsWith('CJ')) {
-                  paarlPlates.push(plates[i]);
-
-              }
-              if (platesForAllTowns[i].startsWith('CA')) {
-                  capeTown.push(plates[i]);
-
-              }
-          } */
-    
-    }
-     function townRadio(town) {
-         if (town === "Bellville") {
-             return bellvillePlates;
-         }
-         if (town === "Cape Town") {
-             return capeTownPlates;
-         }
-         if (town === "Paarl") {
-             return paarlPlates
-         }
-     }
-    return {
-        town: whichTown,
-        townSelection: townRadio
-
-    }
 
 }
