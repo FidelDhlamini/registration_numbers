11df4eacfa2ece66073f62f818218e418f643317
FidelDhlamini <goodkidfidel@gmail.com>
2019-08-21 10:35:49 +0200

    added a test for my filter function

--- test/registration_numbers.test.js
+++ test/registration_numbers.test.js
@@ -11,13 +11,31 @@ describe(" RegistrationCheck function", function () {
 
 
     });
-    
+
     it("should return an error if there no registration number typed in the textbox", function () {
         var instance = RegistrationCheck();
-        assert.equal(false,instance.storePlate(""));
-        assert.equal('Enter a registration number',instance.getMessage());
+        assert.equal(false, instance.storePlate(""));
+        assert.equal('Enter a registration number', instance.getMessage());
 
     });
+
+    it("should return a message if registration number has been added before", function () {
+        var instance = RegistrationCheck();
+        instance.storePlate("CA 654")
+        assert.equal(false, instance.storePlate("CA 654"));
+        assert.equal("Registration number already exists", instance.getMessage());
+
+    });
+
+    it("should return only registration numbers from selected town", function () {
+        var instance = RegistrationCheck();
+        instance.storePlate("CA 654,CL 5648,CJ 567")
+        assert.equal(true, instance.storePlate());
+        assert.equal(["CA 654"], instance.filterRegNum("CA"));
+
+    });
+
+
     //  it("should return an error if there no registration number typed in the textbox", function () {
     //      var instance = RegistrationCheck();
 
